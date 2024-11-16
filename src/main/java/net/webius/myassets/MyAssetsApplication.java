a8f3e745b06283eb22f745592339064a6f5e2244
dev-webius <dev_webius@kakao.com>
2024-11-16 16:13:40 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/net/webius/myassets/MyAssetsApplication.java
@@ -0,0 +1,13 @@
+package net.webius.myassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAssetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAssetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/webius/myassets/MyAssetsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.webius.myassets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAssetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
