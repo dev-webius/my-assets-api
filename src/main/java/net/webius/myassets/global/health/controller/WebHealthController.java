c568c4991936a5f590caea4537e9c14f97f0f9ea
dev-webius <dev_webius@kakao.com>
2024-11-17 11:31:22 +0900

    feat: Web Health 컨트롤러 Swagger 등록

--- src/main/java/net/webius/myassets/global/health/controller/WebHealthController.java
+++ src/main/java/net/webius/myassets/global/health/controller/WebHealthController.java
@@ -1,10 +1,16 @@
 package net.webius.myassets.global.health.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tags({
+        @Tag(name = "Global"),
+        @Tag(name = "Web Health"),
+})
 @RestController
 @RequestMapping("/v1/hello")
 public class WebHealthController {
