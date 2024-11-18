e1e5ed0d9416c75747dfeca56be7ff3f15f6f906
dev-webius <dev_webius@kakao.com>
2024-11-19 07:19:37 +0900

    feat: @Password 제약 조건 추가, 테스트 코드 추가

---
+++ src/main/java/net/webius/myassets/annotation/Password.java
@@ -0,0 +1,19 @@
+package net.webius.myassets.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import net.webius.myassets.validator.PasswordValidator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordValidator.class)
+public @interface Password {
+    String message() default "{validation.constraints.PasswordValidator.message}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
