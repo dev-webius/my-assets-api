0b2f9f7da07aa052057215ce7767b2fcbaa7a670
dev-webius <dev_webius@kakao.com>
2024-11-26 22:28:25 +0900

    feat: AuthProperties record 로 설정, Pbkdf2 관련 속성 추가

--- src/main/java/net/webius/myassets/properties/AuthProperties.java
+++ src/main/java/net/webius/myassets/properties/AuthProperties.java
@@ -1,11 +1,39 @@
 package net.webius.myassets.properties;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "my-assets.auth")
-@RequiredArgsConstructor @Getter
-public class AuthProperties {
-    private final String secret;
+public record AuthProperties(@NotNull String secret, Integer saltLength, Pbkdf2Properties pbkdf2) {
+    public static final Integer DEFAULT_SALT_LENGTH = 64;
+
+    public AuthProperties {
+        if (saltLength == null) {
+            saltLength = DEFAULT_SALT_LENGTH;
+        }
+        if (pbkdf2 == null) {
+            pbkdf2 = new Pbkdf2Properties();
+        }
+    }
+
+    public record Pbkdf2Properties(Integer iterations, SecretKeyFactoryAlgorithm algorithm) {
+        public static final Integer DEFAULT_ITERATIONS = 310000;
+        public static final SecretKeyFactoryAlgorithm DEFAULT_ALGORITHM = SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256;
+
+        public Pbkdf2Properties() {
+            this(DEFAULT_ITERATIONS, DEFAULT_ALGORITHM);
+        }
+
+        public Pbkdf2Properties {
+            if (iterations == null || iterations < 1) {
+                iterations = DEFAULT_ITERATIONS;
+            }
+            if (algorithm == null) {
+                algorithm = DEFAULT_ALGORITHM;
+            }
+        }
+    }
 }
--- src/test/java/net/webius/myassets/properties/AuthPropertiesTest.java
+++ src/test/java/net/webius/myassets/properties/AuthPropertiesTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static net.webius.myassets.properties.AuthProperties.DEFAULT_SALT_LENGTH;
+import static net.webius.myassets.properties.AuthProperties.Pbkdf2Properties.DEFAULT_ITERATIONS;
+import static net.webius.myassets.properties.AuthProperties.Pbkdf2Properties.DEFAULT_ALGORITHM;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest @DisplayName("AuthProperties 테스트")
@@ -16,8 +19,15 @@ public class AuthPropertiesTest {
         this.authProperties = authProperties;
     }
 
-    @Test @DisplayName("secret 필드 검증")
-    public void secretField() {
-        assertThat(authProperties.getSecret()).isEqualTo("test");
+    @Test @DisplayName("기본 필드 검증")
+    public void secretAndSaltFields() {
+        assertThat(authProperties.secret()).isEqualTo("test");
+        assertThat(authProperties.saltLength()).isEqualTo(DEFAULT_SALT_LENGTH);
+    }
+
+    @Test @DisplayName("PBKDF2 필드 검증")
+    public void pbkdf2Fields() {
+        assertThat(authProperties.pbkdf2().iterations()).isEqualTo(DEFAULT_ITERATIONS);
+        assertThat(authProperties.pbkdf2().algorithm()).isEqualTo(DEFAULT_ALGORITHM);
     }
 }
