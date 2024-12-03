a1a9778a9a312bb19798111c68d6580807a9a818
dev-webius <dev_webius@kakao.com>
2024-12-04 07:16:51 +0900

    refactor: 어플리케이션용 WebMvcTest 어노테이션으로 적용

--- src/test/java/net/webius/myassets/global/health/controller/WebHealthControllerTest.java
+++ src/test/java/net/webius/myassets/global/health/controller/WebHealthControllerTest.java
@@ -1,11 +1,9 @@
 package net.webius.myassets.global.health.controller;
 
-import net.webius.myassets.component.MessageSourceProvider;
+import net.webius.myassets.test.annotation.MyAssetsWebMvcTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,8 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@MockBean(MessageSourceProvider.class)
-@WebMvcTest(controllers = WebHealthController.class) @DisplayName("WebHealthController 테스트")
+@MyAssetsWebMvcTest(controllers = WebHealthController.class) @DisplayName("WebHealthController 테스트")
 public class WebHealthControllerTest {
     private final MockMvc mvc;
 
@@ -23,7 +20,6 @@ public class WebHealthControllerTest {
         this.mvc = mvc;
     }
 
-    @WithMockUser
     @Test @DisplayName("웹 서비스 체크")
     public void hello() throws Exception {
         mvc.perform(get("/v1/hello"))
