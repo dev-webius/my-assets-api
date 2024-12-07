ea4dece80c7e4908b31c057a52ae8e4a51d20dfc
dev-webius <dev_webius@kakao.com>
2024-12-07 20:12:05 +0900

    refactor: ManagedException ResponseStatus 변경

--- src/main/java/net/webius/myassets/exception/ManagedException.java
+++ src/main/java/net/webius/myassets/exception/ManagedException.java
@@ -3,7 +3,7 @@ package net.webius.myassets.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class ManagedException extends RuntimeException {
     public ManagedException(String message) {
         super(message);
