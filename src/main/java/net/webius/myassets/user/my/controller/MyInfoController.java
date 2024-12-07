7bf48a9b5dfd77856a48662a028e9c788104d0da
dev-webius <dev_webius@kakao.com>
2024-12-08 03:34:23 +0900

    add: MyInfo 서비스 구현

---
+++ src/main/java/net/webius/myassets/user/my/controller/MyInfoController.java
@@ -0,0 +1,37 @@
+package net.webius.myassets.user.my.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.webius.myassets.user.my.dto.ApplyMyInfoReq;
+import net.webius.myassets.user.my.dto.MyInfoRes;
+import net.webius.myassets.user.my.service.MyInfoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "User")
+@Tag(name = "My")
+@RestController @RequiredArgsConstructor
+@RequestMapping("/v1/my/info")
+public class MyInfoController {
+    private final MyInfoService myInfoService;
+
+    @GetMapping
+    @Operation(summary = "내 정보 불러오기")
+    @ApiResponse(responseCode = "200", description = "성공")
+    @ApiResponse(responseCode = "401", description = "권한 없음")
+    public ResponseEntity<MyInfoRes> getMyInfo() {
+        var myInfo = myInfoService.getMyInfo();
+        return ResponseEntity.ok(myInfo);
+    }
+
+    @PutMapping
+    @Operation(summary = "내 정보 수정하기")
+    @ApiResponse(responseCode = "204", description = "성공")
+    public ResponseEntity<Void> applyMyInfo(@Valid @RequestBody ApplyMyInfoReq applyMyInfoReq) {
+        myInfoService.applyMyInfo(applyMyInfoReq);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/net/webius/myassets/user/my/dto/ApplyMyInfoReq.java
@@ -0,0 +1,15 @@
+package net.webius.myassets.user.my.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+public class ApplyMyInfoReq {
+    @NotNull
+    @PastOrPresent
+    private LocalDate birthday;
+}
