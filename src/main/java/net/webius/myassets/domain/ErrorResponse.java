ce94009e1e2510c93af57484d177f0c6508d4cf7
dev-webius <dev_webius@kakao.com>
2024-12-07 20:06:36 +0900

    clean: 사용 하지 않는 import 정리

--- src/main/java/net/webius/myassets/domain/ErrorResponse.java
+++ src/main/java/net/webius/myassets/domain/ErrorResponse.java
@@ -3,8 +3,6 @@ package net.webius.myassets.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Arrays;
-
 @Getter @Setter
 public class ErrorResponse {
     private String message;
--- src/test/java/net/webius/myassets/global/health/controller/WebHealthControllerTest.java
+++ src/test/java/net/webius/myassets/global/health/controller/WebHealthControllerTest.java
@@ -4,7 +4,6 @@ import net.webius.myassets.test.annotation.MyAssetsWebMvcTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
