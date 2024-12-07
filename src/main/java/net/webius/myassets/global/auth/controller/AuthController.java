7e4488439f2df553d064307e0ec98b40339e1c22
dev-webius <dev_webius@kakao.com>
2024-12-07 20:08:10 +0900

    refactor: 컨트롤러 swagger 태그 정리

--- src/main/java/net/webius/myassets/global/auth/controller/AuthController.java
+++ src/main/java/net/webius/myassets/global/auth/controller/AuthController.java
@@ -3,8 +3,8 @@ package net.webius.myassets.global.auth.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.tags.Tags;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import net.webius.myassets.global.auth.dto.LoginReq;
 import net.webius.myassets.global.auth.dto.SignupReq;
 import net.webius.myassets.global.auth.dto.UserSessionRes;
@@ -18,26 +18,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tags({
-        @Tag(name = "All"),
-        @Tag(name = "Global"),
-        @Tag(name = "Auth"),
-})
-@RestController
+@Tag(name = "Global")
+@Tag(name = "Auth")
+@RestController @RequiredArgsConstructor
 @RequestMapping("/v1/auth")
 public class AuthController {
     private final LoginService loginService;
     private final SignupService signupService;
     private final JwtService jwtService;
 
-    public AuthController(LoginService loginService, SignupService signupService, JwtService jwtService) {
-        this.loginService = loginService;
-        this.signupService = signupService;
-        this.jwtService = jwtService;
-    }
-
     @PostMapping("/login")
     @Operation(summary = "로그인")
+    @ApiResponse(responseCode = "200", description = "로그인 성공")
+    @ApiResponse(responseCode = "400", description = "사용자 인증 실패")
     public ResponseEntity<UserSessionRes> login(@RequestBody @Valid LoginReq loginReq) {
         loginService.login(loginReq);
         var response = jwtService.createUserSession(loginReq.getUsername());
@@ -48,7 +41,8 @@ public class AuthController {
 
     @PostMapping("/signup")
     @Operation(summary = "회원가입")
-    @ApiResponse(responseCode = "201", description = "성공")
+    @ApiResponse(responseCode = "201", description = "회원가입 성공")
+    @ApiResponse(responseCode = "400", description = "사용자 등록 실패")
     public ResponseEntity<UserSessionRes> signup(@RequestBody @Valid SignupReq signupReq) {
         signupService.signup(signupReq);
         var response = jwtService.createUserSession(signupReq.getUsername());
--- src/main/java/net/webius/myassets/global/health/controller/WebHealthController.java
+++ src/main/java/net/webius/myassets/global/health/controller/WebHealthController.java
@@ -5,17 +5,13 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tags({
-        @Tag(name = "All"),
-        @Tag(name = "Global"),
-        @Tag(name = "Web Health"),
-})
+@Tag(name = "Global")
+@Tag(name = "Web Health")
 @RestController
 @RequestMapping("/v1/hello")
 public class WebHealthController {
